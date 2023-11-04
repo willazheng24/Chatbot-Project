28761f9281f6fcc6ca625d060e5e1b140970b016
willazheng24 <wzheng96@gatech.edu>
2023-11-03 23:32:44 -0400

    comments

--- botTemplate/public/script.js
+++ botTemplate/public/script.js
@@ -1,7 +1,9 @@
 document.getElementById('chatForm').addEventListener('submit', async function (event) {
     event.preventDefault();
+    //stores and displays user input
     const userInput = document.getElementById('userInput').value;
     document.getElementById('inputText').textContent = userInput;
+    //what taylor's chat says while loading
     document.getElementById('chatOutput').textContent = "Taylor is writing...";
     const response = await fetch('/api/chat', {
         method: 'POST',
@@ -12,7 +14,9 @@ document.getElementById('chatForm').addEventListener('submit', async function (e
     });
 
     const data = await response.json();
+    //displays ai output
     document.getElementById('chatOutput').textContent = data.message;
+    //formatting the text boxes so they are the same height
     const elementHeight = document.getElementById('chatBox').clientHeight;
     document.getElementById('inputBox').style.height = elementHeight + "px"; 
 });
